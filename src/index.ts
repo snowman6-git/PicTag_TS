28aad64f65ef0d7779fa536ef46f1abf5b52a75b
snowman6-git <chldlfgks55@gmail.com>
2025-01-28 02:41:05 +0900

    경로문제, 패키지 문제

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono'
-import { join } from 'path';
-import { serveStatic } from 'hono/bun'
 import { file } from 'bun';
+import path = require("path")
 
 const app = new Hono()
-app.use('./images/*', serveStatic({ root: './' }))
 
 app.get('/', (c) => {
   return c.text('welcome! this is personal image server!')
@@ -13,10 +11,11 @@ app.get('/', (c) => {
 app.get('/image', async (c) => {
   const image = c.req.query('tag')?.toString() || "../"
   const jiral_no = !/[\.]{2,}|[\/\\]/.test(image) //.이 두개거나, /or\가 파라미터에 있다면 true /양옆에 안하면 씹힘
+  console.log(jiral_no)
   if(jiral_no){
-    const image_root = join(process.cwd(), `./images/${image}`); // 파일 경로 설정
-    return new Response(file(image_root)); 
-  } else { return new Response(file(join(process.cwd(), `./images/don.webp`))) } //자신이 원하는 비정상 접근 대응
+    const image_root = path.join(__dirname, '../images', image);
+    return new Response(file(image_root));
+  } else { return new Response(file(path.join(__dirname, '../images/don.webp'))) } //자신이 원하는 비정상 접근 대응
 });
 
 export default { 
